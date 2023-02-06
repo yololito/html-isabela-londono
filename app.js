6da2d8888557b8fe6d1973791b5f740a638a3b10
lolaa <123569523+yololito@users.noreply.github.com>
2023-02-06 12:05:46 -0500

    6 febrero

---
+++ app.js
@@ -0,0 +1,60 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cardsAdj = [
+    {
+      name: "spiderman",
+      imagenes: "imagenes/spiderman.jpg"
+    },
+    {
+      name: "spiderman2",
+      imagenes: "imagenes/spiderman.jpg"
+    },
+    {
+      name: "spiderman",
+      imagenes: "imagenes/spiderman2.jpg"
+    },
+    {
+      name: "spiderman3",
+      imagenes: "imagenes/spiderman3.jpg"
+    },
+    {
+      name: "spiderman4",
+      imagenes: "imagenes/spiderman4.jpg"
+    },
+    {
+      name: "spiderman5",
+      imagenes: "imagenes/spiderman5.jpg"
+    },
+    {
+      name: "spiderman6",
+      imagenes: "imagenes/spiderman6.jpg"
+    },
+    {
+      name: "spiderman",
+      imagenes: "imagenes/spiderman.jpg"
+    },
+    {
+      name: "spiderman2",
+      imagenes: "imagenes/spiderman.jpg"
+    },
+    {
+      name: "spiderman",
+      imagenes: "imagenes/spiderman2.jpg"
+    },
+    {
+      name: "spiderman3",
+      imagenes: "imagenes/spiderman3.jpg"
+    },
+    {
+      name: "spiderman4",
+      imagenes: "imagenes/spiderman4.jpg"
+    },
+    {
+      name: "spiderman5",
+      imagenes: "imagenes/spiderman5.jpg"
+    },
+    {
+      name: "spiderman6",
+      imagenes: "imagenes/spiderman6.jpg"
+    }
+  ];
+});
