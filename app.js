94cfddf7d75c1e5cbe907be3be76b8cbc3833a6e
lolaa <123569523+yololito@users.noreply.github.com>
2023-03-08 09:18:28 -0500

    8 de marzo

--- app.js
+++ app.js
@@ -64,3 +64,26 @@ const resultado = document.querySelector("#resultado");
 var cartasEscogidas = [];
 var cartasEscogidasId = [];
 var cartasGanadas = [];
+
+//----------------- lecture_03 ----------------------------------//
+function crearTablero() {
+  for (let i = 0; i < cardAdj.length; i++) {
+    var carta = document.createElement("img");
+    carta.setAttribute("src", "imagenes/reverso.png");
+
+    carta.setAttribute("data-id", i);
+    carta.addEventListener("click", voltearCarta);
+    cuadricula.appendChild(carta);
+  }
+}
+fuction voltearCarta(){
+  var cardId = this.getAttribute("data-id");
+  cartasEscogidas.push(cardAdj[cardId].name);
+  cartasEscogidasId.push(cardId);
+  this.setAttribute("src", cardAdj[cardId].img);
+  if (cartasEscogidas.length === 2){
+    setTimeout(verificarPareja, 1000);
+  }
+}
+crearTablero();
+});
